e86d20c52ea967befa6bb7e398e586655a1e097e
sollar <sollar.2003.2013@gmail.com>
2026-02-03 20:56:40 +0900

    JDBC task completed

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -12,115 +12,37 @@ import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
-        createUsersTable();
+
     }
 
 
     @Override
     public void createUsersTable() {
-        String sql = "CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(45), lastName VARCHAR(45), age TINYINT)";
-
-
-
-
-
-
-
-
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-
-            statement.executeUpdate(sql);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
 
     }
 
     @Override
     public void dropUsersTable() {
-        String sql = "DROP TABLE IF EXISTS users";
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-
-            statement.executeUpdate(sql);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        String sql = String.format(
-                "INSERT INTO users (name, lastName, age) VALUES ('%s', '%s', %d)",
-                name, lastName, age
-        );
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-
-            statement.executeUpdate(sql);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
 
     }
 
     @Override
     public void removeUserById(long id) {
-        String sql = "DELETE FROM users WHERE id = " + id;
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-
-            statement.executeUpdate(sql);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
-
-        String sql = "SELECT id, name, lastName, age FROM users";
-
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(sql)) {
-
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastName"));
-                user.setAge(resultSet.getByte("age"));
-
-                users.add(user);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return users;
+        return new ArrayList<>();
     }
 
     @Override
     public void cleanUsersTable() {
-        String sql = "DELETE FROM users";
 
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement()) {
-
-            statement.executeUpdate(sql);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
