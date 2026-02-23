4e5f216bde559f5217eb640dbdbb62547a0d2c2c
sollar35 <sollar.2003.2013@gmail.com>
2026-02-23 20:13:29 +0900

    true and false

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,11 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+
+    @Scope("prototype")
+    @Bean(name="cat")
+    public Cat getCat() {
+        return new Cat();
+    }
 }
