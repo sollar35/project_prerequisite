56e4aa13d2bd779705c5e994be4504c81ab5150b
sollar35 <sollar.2003.2013@gmail.com>
2026-03-14 20:30:03 +0900

    user car

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,44 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String model;
+
+    @Column
+    private int series;
+
+    public Car() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+}
